1e5b07a6b9508fdcc16c6b1dcf1cfcb69f9fd948
Ignatiuschrist <ignatiuschristabel134@gmail.com>
2025-01-13 00:29:28 +0700

    change baseUrl to deployed be

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"http://api.dailyjournal.my.id/api",
   timeout: 50000,
 });
 
